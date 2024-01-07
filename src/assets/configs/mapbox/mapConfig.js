766f5fe0bc72117161d6ca03b3f1ec2d48dba700
xin1602 <christinelee1114@gmail.com>
2024-01-07 17:49:38 +0800

    設定符合桃園的地圖中心與邊界

--- src/assets/configs/mapbox/mapConfig.js
+++ src/assets/configs/mapbox/mapConfig.js
@@ -2,15 +2,51 @@
 export const MapObjectConfig = {
 	antialias: true,
 	container: "mapboxBox",
-	center: [121.536609, 25.044808],
+	center: [121.251374, 24.932593],
 	maxBounds: [
-		[121.3870596781498, 24.95733863075891], // Southwest coordinates
-		[121.6998231749096, 25.21179993640203], // Northeast coordinates
+		[121.0970596781498, 24.65733863075891], // Southwest coordinates
+		[121.4198231749096, 25.11179993640203], // Northeast coordinates
 	],
 	zoom: 12.5,
 	minZoom: 11,
 	maxZoom: 22,
 	projection: "globe", // display the map as a 3D globe
+};
+
+// Styles for base layer "Taoyuan Town"
+export const TaoyuanTown = {
+	id: "taoyuan_town",
+	source: "taoyuan_town",
+	type: "symbol",
+	layout: {
+		"text-field": ["to-string", ["get", "TNAME"]],
+		"text-size": [
+			"interpolate",
+			["linear"],
+			["zoom"],
+			11,
+			10,
+			13,
+			12,
+			15.5,
+			14,
+		],
+		"text-allow-overlap": true,
+	},
+	paint: {
+		"text-color": [
+			"interpolate",
+			["linear"],
+			["zoom"],
+			15,
+			"#aaa",
+			16,
+			"#fff",
+		],
+		"text-halo-color": "#888",
+		"text-halo-width": ["interpolate", ["linear"], ["zoom"], 15, 0, 16, 1],
+		"text-opacity": ["interpolate", ["linear"], ["zoom"], 15.99, 1, 16, 0],
+	},
 };
 
 // Styles for base layer "Taipei Town"
